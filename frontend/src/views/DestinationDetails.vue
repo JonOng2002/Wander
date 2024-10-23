77613fabeff5ada7d54117777a51bc52226427c0
Wong Jia XIn <jiaxin.wong.2023@scis.smu.edu.sg>
2024-10-23 15:28:55 +0800

    modified destination details

--- frontend/src/views/DestinationDetails.vue
+++ frontend/src/views/DestinationDetails.vue
@@ -54,7 +54,7 @@ export default {
       attractions: [],
       loading: true,
       country: this.$route.params.country,
-      apiKey: 'YOUR_API_KEY',
+      apiKey: 'AIzaSyCv4guJix6s5zFZjK2GokfshsfqlLAU3Lg',
       cityName: this.$route.params.city || 'Unknown City',
       userId: null,
       showPopup: false,
